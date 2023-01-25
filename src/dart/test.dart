4eb97eecf53280bad7e3dc0b90b57ceff917e3b7
ltOgt <omnesiaorg@gmail.com>
2023-01-25 13:12:35 +0100

    add more common patterns to test.dart

--- src/dart/test.dart
+++ src/dart/test.dart
@@ -3,11 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Class', () {
-    test('function', () async {
+    test('throws', () async {
       expect(() => throw Exception(), throwsA(isA<Exception>()));
     });
+    test('does not throw', () async {
+      expect(() => 5, returnsNormally);
+    });
+
 		testWidgets("widget", (tester) async {
       await tester.pumpWidget(Container());
+			await tester.pumpAndSettle();
+			await tester.binding.setSurfaceSize(kScreenSize);
+			await tester.drag(find.byKey(dragKey), moveByOffset);
     });
   });
 }
